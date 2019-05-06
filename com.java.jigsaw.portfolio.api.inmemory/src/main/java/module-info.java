f89b438cd189fc8105a2e96a8f70cf54536e253a
Baptiste Meynier <baptiste.meynier-ext@amundi.com>
2019-05-06 14:09:51 +0200

    correct maven settings

--- com.java.jigsaw.portfolio.api.inmemory/src/main/java/module-info.java
+++ com.java.jigsaw.portfolio.api.inmemory/src/main/java/module-info.java
@@ -1,4 +1,4 @@
 module com.java.jigsaw.portfolio.api.inmemory {
     requires com.java.jigsaw.portfolio.spi;
-    provides com.java.jigsaw.portfolio.spi with com.java.jigsaw.portfolio.api.InMemoryPortfolioService;
+    provides com.java.jigsaw.portfolio.spi.PortfolioServicePort with com.java.jigsaw.portfolio.api.InMemoryPortfolioService;
 }
