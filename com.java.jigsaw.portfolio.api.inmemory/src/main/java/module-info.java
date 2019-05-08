744ea5f780e32bd631d7f91a8f347fc2d32f115c
BaptisteMeynier <baptiste.meynier@gmail.com>
2019-05-08 20:33:58 +0200

    adding inMemory-launcher

--- com.java.jigsaw.portfolio.api.inmemory/src/main/java/module-info.java
+++ com.java.jigsaw.portfolio.api.inmemory/src/main/java/module-info.java
@@ -1,6 +1,5 @@
-import com.java.jigsaw.portfolio.api.inmemory.InMemoryPortfolioService;
 
 module com.java.jigsaw.portfolio.api.inmemory {
     requires com.java.jigsaw.portfolio.spi;
-    provides com.java.jigsaw.portfolio.spi.PortfolioServicePort with InMemoryPortfolioService;
+    provides com.java.jigsaw.portfolio.spi.PortfolioServicePort with com.java.jigsaw.portfolio.api.inmemory.InMemoryPortfolioService;
 }
